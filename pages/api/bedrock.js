04bda47375f2af22f98f585033e15c4b84671abf
Christopher Gomez <chris.gomez@pcrichard.com>
2024-04-26 15:23:31 -0400

    Add results to end

--- pages/api/bedrock.js
+++ pages/api/bedrock.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
 	console.log("req", req);
 	// const result = await bedrockconnection;
-	res.status(200).json({ pokemon: req.query.pokemon });
+	res.status(200).json([req.query.pokemon]);
 }
